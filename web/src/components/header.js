7ae35688374fc5b6a5a5e7c0dd998e16618f5d85
Tanya Mott <tlm.atlga@gmail.com>
2019-08-17 01:48:57 -0400

    build fix

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -2,14 +2,18 @@ import {Link} from 'gatsby'
 import React from 'react'
 import Icon from './icon'
 import {cn} from '../lib/helpers'
+import logo from '../images/yummyeats.png';
 
 import styles from './header.module.css'
+
+var divStyle = {width:'50%'};
+
 
 const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
   <div className={styles.root}>
     <div className={styles.wrapper}>
       <div className={styles.branding} class="text-center">
-        <Link to='/'><img src="../src/images/yummyeats.png" style="width:50%;"/></Link>
+        <Link to='/'><img src={ logo } style = {divStyle}/></Link>
       </div>
 
       <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
