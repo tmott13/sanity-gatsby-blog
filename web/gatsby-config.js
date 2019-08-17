15de4df2227527ce2c5e9917fd075bb552d036e8
Tanya Mott <tlm.atlga@gmail.com>
2019-08-17 11:22:30 -0400

    SEO

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -20,5 +20,12 @@ module.exports = {
         overlayDrafts: !isProd
       }
     }
-  ]
+  ],
+  siteMetadata: {
+    title: "Yummy Eats ATL",
+    description:
+      "Yummy Eats ATL was made to show off some of the best authentic/traditional restaurants in the Atlanta area.",
+    url: "https://yummyeatsatl.fun", // No trailing slash allowed!
+    image: "/images/yummyeats.png" // Path to your image you placed in the 'static' folder
+  }
 }
